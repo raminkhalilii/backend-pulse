640306c47134522b15e5c0029dc3049d552cee11
Ramin Khalili <ramin.khalili@studenti.unipd.it>
2026-02-19 20:26:04 +0100

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+
+export default tseslint.config(
+    {
+        ignores: ['eslint.config.mjs'],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginUnicorn.configs.all,
+    eslintPluginPrettierRecommended,
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/explicit-module-boundary-types': 'error',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            "prettier/prettier": ["error", {endOfLine: "auto"}],
+        },
+    },
+);
