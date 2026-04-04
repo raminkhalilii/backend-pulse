3b7f0b2bdda45aced1a58724f7fedd8720744800
Ramin Khalili <ramin.khalili@studenti.unipd.it>
2026-04-04 12:14:21 +0200

    fix: set SameSite attribute to 'lax' for refreshToken cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,7 +18,7 @@ export class AuthController {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   }
